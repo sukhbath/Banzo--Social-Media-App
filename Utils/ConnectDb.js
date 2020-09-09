e4124312b93cede1bafb9ce9ac88d48ddf2538e1
Sukhraj Singh <43281127+sukhbath@users.noreply.github.com>
2020-09-09 09:13:54 -0400

    User Model

---
+++ Utils/ConnectDb.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+module.exports = function (dbLink) {
+  mongoose
+    .connect(dbLink, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(function (v) {
+      console.log("::::::::::::::::::::::::::::::::::::::::::::");
+      console.log(":::::::::::::DATABASE CONNECTED:::::::::::::");
+      console.log("::::::::::::::::::::::::::::::::::::::::::::");
+    })
+    .catch(function (error) {
+      console.log("::::::::::::::::::::::::::::::::::::::::::::");
+      console.log(":::::::::DATABASE CONNECTION FAILED:::::::::");
+      console.log(error);
+    });
+};
--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-mongoose
-  .connect(
-    "mongodb+srv://sukh:sukh@cluster0-7nszg.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(function (v) {
-    console.log("::::::::::::::::::::::::::::::::::::::::::::");
-    console.log(":::::::::::::DATABASE CONNECTED:::::::::::::");
-    console.log("::::::::::::::::::::::::::::::::::::::::::::");
-  })
-  .catch(function (error) {
-    console.log("::::::::::::::::::::::::::::::::::::::::::::");
-    console.log(":::::::::DATABASE CONNECTION FAILED:::::::::");
-    console.log(error);
-  });
+const UserModel = require("./Models/USER_MODEL");
+const ConnectDb = require("./Utils/ConnectDb");
 
-app.get("/", (req, res) => {
-  res.send("Banzo App");
-});
+var UserController = require("./Controllers/USER_CONTROLLER");
+
+ConnectDb(
+  "mongodb+srv://sukh:sukh@cluster0-7nszg.mongodb.net/<dbname>?retryWrites=true&w=majority"
+);
+
+app.use(express.json());
+
+app
+  .route("/api/v1/users")
+  .get(UserController.getUsers)
+  .post(UserController.createUser);
 
 app.listen(3000, function () {
   console.log("Server has been started at 3000\n");
