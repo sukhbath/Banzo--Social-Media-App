3ca0e5c63ed76b0e11b78a872e445621ef47f15b
Sukhraj Singh <43281127+sukhbath@users.noreply.github.com>
2020-09-09 08:19:07 -0400

    DB Connection💾

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://sukh:sukh@cluster0-7nszg.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(function (v) {
+    console.log("::::::::::::::::::::::::::::::::::::::::::::");
+    console.log(":::::::::::::DATABASE CONNECTED:::::::::::::");
+    console.log("::::::::::::::::::::::::::::::::::::::::::::");
+  })
+  .catch(function (error) {
+    console.log("::::::::::::::::::::::::::::::::::::::::::::");
+    console.log(":::::::::DATABASE CONNECTION FAILED:::::::::");
+    console.log(error);
+  });
 
 app.get("/", (req, res) => {
   res.send("Banzo App");
 });
 
 app.listen(3000, function () {
-  console.log("Server has been started at 3000");
+  console.log("Server has been started at 3000\n");
 });
