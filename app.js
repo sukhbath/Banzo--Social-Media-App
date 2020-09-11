0cfd75ed37f546416a8ed4a1a0e6a984fdba4e0d
Sukhraj Singh <43281127+sukhbath@users.noreply.github.com>
2020-09-11 09:34:21 -0400

    User Routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const UserModel = require("./Models/USER_MODEL");
 const ConnectDb = require("./Utils/ConnectDb");
 const ErrorController = require("./Controllers/ERROR_CONTROLLER");
+const UserRoute = require("./Routes/USER_ROUTE");
+const UserModel = require("./Models/USER_MODEL");
 
 var UserController = require("./Controllers/USER_CONTROLLER");
 
@@ -12,16 +13,19 @@ ConnectDb(
 
 app.use(express.json());
 
-app
-  .route("/api/v1/users")
-  .get(UserController.getUsers)
-  .post(UserController.createUser);
+app.use("/api/v1/users", UserRoute);
 
-app
-  .route("/api/v1/users/:id")
-  .get(UserController.getUser)
-  .patch(UserController.updateUser)
-  .delete(UserController.deleteUser);
+// Posts
+// app
+//   .route("/api/v1/posts")
+//   .get(UserController.getUsers)
+//   .post(UserController.createUser);
+
+// app
+//   .route("/api/v1/posts/:id")
+//   .get(UserController.getUser)
+//   .patch(UserController.updateUser)
+//   .delete(UserController.deleteUser);
 
 app.use(ErrorController);
 
